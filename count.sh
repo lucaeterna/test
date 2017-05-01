d61aabb0342952f27e54081ccadcff733c17ec79
lucaeterna <kazakova1537@gmail.com>
2017-05-01 16:41:37 +0300

    counting functions

--- count.sh
+++ count.sh
@@ -1,50 +1,48 @@
 #! /bin/bash
 
-
-
 count_size_bytes () { #arg1 = directory
-  
+
   sum=0
-  dir=$1
-  for n in `find $dir -exec stat -c%s {} \;`; do let "sum+=n" ; done
+  dir="$1"
+  for n in $(find $dir -exec stat -c%s {} +); do let "sum+=n" ; done
   echo $sum
+
 }
+
 
 count_size_kb () { #arg1 = directory
-  
+
   sum=0
-  dir=$1
-  for n in `find $dir -exec stat -c%b {} \;`; do let "sum+=n" ; done
+  dir="$1"
+  for n in $(find $dir -exec stat -c%b {} +); do let "sum+=n" ; done
   let "sum+=sum/2"
   echo $sum
+
 }
 
+
 count_size () { #arg1 = directory
-  
+
   sum=0
-  dir=$1
+  dir="$1"
   declare -a arr
 
   while IFS= read -r -d '' file; do
-    #echo "$file"
-    h=`stat -c%h "$file"`
-    size=`stat -c%b "$file"`
-    #echo "$size"
+    h="$(stat -c%h "$file")"
+    size="$(stat -c%b "$file")"
     if [[ $h -gt 1 ]]; then 
-      i=`stat -c%i $file`
+      i="$(stat -c%i $file)"
       if ! [[ ${arr[$i]} ]]; then
-        #echo "This file has been already counted !"
         arr[$i]=1
         let "sum+=size"
-      else
-        echo "This file was already counted"
-        #arr[$i]=1
-        #let "sum+=size"
+      #else
+      #  echo "This file has already been counted"
       fi
     else
       let "sum+=size"
     fi
   done < <(find $dir -print0)  
+
   let "sum=sum/2"
   echo "$sum"
 
